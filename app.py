cd3e5a4a035c303880672a0129a282b00c486493
Paulo <paulosesisenai2024@gmail.com>
2025-03-25 14:47:10 -0300

    Terceiro Commit

--- app.py
+++ app.py
@@ -47,16 +47,16 @@ def medio():
     global nome
     global imagem
     if request.method == 'GET':
-        aleatorio = random.randint(0, 151)
+        aleatorio = random.randint(0, 251)
         API_ENDPOINT = f"https://pokeapi.co/api/v2/pokemon/{aleatorio}"
         response = requests.get(API_ENDPOINT)
         if response.status_code == 200:
             data = response.json()
             nome = data["forms"][0]["name"]
             imagem = data["sprites"]["other"]["official-artwork"]["front_default"]
-            return render_template("facil.html", nome=nome, imagem=imagem)
+            return render_template("medio.html", nome=nome, imagem=imagem)
 
-        return render_template("facil.html", nome=nome, imagem=imagem)
+        return render_template("medio.html", nome=nome, imagem=imagem)
 
         return "Erro ao buscar dados da API", 500
 
@@ -71,23 +71,23 @@ def medio():
                 msg = f"❌ Errado! O nome é {nome.capitalize()}."
                 
 
-            return render_template("facil.html", nome=nome, imagem=imagem, msg=msg)
+            return render_template("medio.html", nome=nome, imagem=imagem, msg=msg)
 
 @app.route('/dificil', methods=['GET', 'POST'])
 def dificil():
     global nome
     global imagem
     if request.method == 'GET':
-        aleatorio = random.randint(0, 151)
+        aleatorio = random.randint(0, 1025)
         API_ENDPOINT = f"https://pokeapi.co/api/v2/pokemon/{aleatorio}"
         response = requests.get(API_ENDPOINT)
         if response.status_code == 200:
             data = response.json()
             nome = data["forms"][0]["name"]
             imagem = data["sprites"]["other"]["official-artwork"]["front_default"]
-            return render_template("facil.html", nome=nome, imagem=imagem)
+            return render_template("dificil.html", nome=nome, imagem=imagem)
 
-        return render_template("facil.html", nome=nome, imagem=imagem)
+        return render_template("dificil.html", nome=nome, imagem=imagem)
 
         return "Erro ao buscar dados da API", 500
 
@@ -97,12 +97,10 @@ def dificil():
         if resposta:  # Só faz a verificação se algo foi digitado
             if resposta == nome.lower():
                 msg = "✅ Você Acertou! 🎉"
-                
             else:
                 msg = f"❌ Errado! O nome é {nome.capitalize()}."
-                
 
-            return render_template("facil.html", nome=nome, imagem=imagem, msg=msg)
+            return render_template("dificil.html", nome=nome, imagem=imagem, msg=msg)
 
 
 if __name__ == '__main__':
